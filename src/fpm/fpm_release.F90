c43d33471718b0077454e707e7520d98cdaaa216
jinangshah21 <jinang2110@gmail.com>
2025-04-04 00:58:30 +0530

    XX: remove cpp comments in c preprocessing

--- src/fpm/fpm_release.F90
+++ src/fpm/fpm_release.F90
@@ -24,10 +24,10 @@ module fpm_release
 ! Accept solution from https://stackoverflow.com/questions/31649691/stringify-macro-with-gnu-gfortran
 ! which provides the "easiest" way to pass a macro to a string in Fortran complying with both
 ! gfortran's "traditional" cpp and the standard cpp syntaxes
-#ifdef __GFORTRAN__ /* traditional-cpp stringification */
+#ifdef __GFORTRAN__ 
 #  define STRINGIFY_START(X) "&
 #  define STRINGIFY_END(X) &X"
-#else               /* default stringification */
+#else             
 #  define STRINGIFY_(X) #X
 #  define STRINGIFY_START(X) &
 #  define STRINGIFY_END(X) STRINGIFY_(X)

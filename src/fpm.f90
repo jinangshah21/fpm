c90363d812c1b832842d213a16463abcfd9ed8cb
jinangshah21 <jinang2110@gmail.com>
2025-07-24 17:43:22 +0530

    chore: adjustments to compile with GFortran

--- src/fpm.f90
+++ src/fpm.f90
@@ -652,7 +652,8 @@ subroutine cmd_run(settings,test)
                 exe_source => exe_target%dependencies(1)%ptr%source
 
                 if (exe_source%unit_scope == run_scope) then
-                    write(stderr,'(A)',advance=(merge("yes","no ",modulo(jj,nCol)==0))) basename(exe_target%output_file, suffix=.false.)
+                    write(stderr,'(A)',advance=(merge("yes","no ",modulo(jj,nCol)==0))) &
+                    basename(exe_target%output_file, suffix=.false.)
                     jj = jj + 1
                 end if
             end if
@@ -667,7 +668,8 @@ subroutine cmd_run(settings,test)
         nCol = LINE_WIDTH/col_width
         write(stderr,*) 'Matched names:'
         do ii=1,size(executables)
-            write(stderr,'(A)',advance=(merge("yes","no ",modulo(jj,nCol)==0))) basename(executables(ii)%s, suffix=.false.)
+            write(stderr,'(A)',advance=(merge("yes","no ",modulo(jj,nCol)==0)))&
+             basename(executables(ii)%s, suffix=.false.)
             jj = jj + 1
         end do
         write(stderr,*)
